20507402b7f4a5d3387879725023ba157a9d159d
Andrew Lee <leec16@wit.edu>
2020-06-17 01:12:59 -0400

    commit on Master

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class Task(models.Model):
+    taskname= models.CharField(max_length=100)
+    tasktype= models.CharField(max_length=100)
+    duedate= models.DateField()
+    estimatetime= models.PositiveIntegerField()
+    taskuser =models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.taskname
+
+    def get_absolute_url(self):
+        return reverse('task-detail',kwargs={'pk':self.pk})
--- main/views.py
+++ main/views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView,CreateView, UpdateView, DeleteView
+from .models import Task
 # Create your views here.
-
 def home(request):
-    return render(request,'main/home.html')
+    context = {
+        'tasks': Task.objects.all()
+    }
+    return render(request,'main/home.html',context)
+
+class TaskListView(ListView):
+    model = Task
+    template_name='main/home.html'
+    context_object_name = 'tasks'
+    ordering =['-duedate']
+
+class TaskDetailView(DetailView):
+    model = Task
+
+class TaskCreateView(LoginRequiredMixin,CreateView):
+    model = Task
+    fields=['taskname','tasktype','duedate','estimatetime']
+
+    def form_valid(self,form):
+        form.instance.taskuser = self.request.user
+        return super().form_valid(form)
+
+class TaskUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView,):
+    model = Task
+    fields=['taskname','tasktype','duedate','estimatetime']
+
+    def form_valid(self,form):
+        form.instance.taskuser = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        task = self.get_object()
+        if self.request.user == task.taskuser:
+            return True
+        return False
+
+class TaskDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Task
+    success_url='/'
+
+    def test_func(self):
+        task = self.get_object()
+        if self.request.user == task.taskuser:
+            return True
+        return False
